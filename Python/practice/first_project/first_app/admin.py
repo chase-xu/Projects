4fbc38a0998a30d53acaa09d823a57add33ba9a2
Chase <PengXu1993@gmail.com>
2019-08-26 18:56:34 -0700

    3

--- Python/practice/first_project/first_app/admin.py
+++ Python/practice/first_project/first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from first_app.models import AccessRecord, Topic, Webpage
+# Register your models here. xp 123456541
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
---
+++ Python/practice/first_project/populate_first_app.py
@@ -0,0 +1,41 @@
+import os  
+os.environ.setdefault('DJANGO_SETTING_MODULE', 'first_project.settings')
+
+import django
+django.setup()
+
+###Fake script
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search',
+    'Social',
+    'Marketplace',
+    'News',
+    'Games',
+]
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+
+    for entry in range(N):
+        ##get topic for entry
+        top = add_topic()
+
+        #create the fake data for the entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        #create a fake access
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
